185c3563a4f9e7e408f8bc7eb53fcc8ad3a106f2
Matt Vaughn <vaughn@tacc.utexas.edu>
2017-10-14 00:45:39 -0500

    Updated sailfish to support an additional flag as per Ginkgo

--- reactors/sailfish/sailfish-0.10.1/runner-template.sh
+++ reactors/sailfish/sailfish-0.10.1/runner-template.sh
@@ -5,4 +5,4 @@ CONTAINER_IMAGE="index.docker.io/sd2e/sailfish:0.10.1--1"
 threads=19
 
 container_exec ${CONTAINER_IMAGE} sailfish index -t ${transcripts} -o ${output}
-container_exec ${CONTAINER_IMAGE} sailfish quant -i ${output} -1 ${fastq1} -2 ${fastq2} -o ${output} ${libtype}
+container_exec ${CONTAINER_IMAGE} sailfish quant ${vbopt} -i ${output} -1 ${fastq1} -2 ${fastq2} -o ${output} ${libtype}
