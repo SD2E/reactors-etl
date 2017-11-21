5d9fd70608d4f046f2714bbbfcfc9731e9dcf18d
Ubuntu <ubuntu@tasbe>
2017-11-21 22:47:46 +0000

    first commit

--- reactors/fcs-tasbe/src/create_batch_histogram_output_object.m
+++
@@ -1,7 +0,0 @@
-function [output_object] = create_batch_histogram_output_object(plots,plots_folder,file,title)
-  output_object.type = "batch_histogram"
-  output_object.plots = plots
-  output_object.plots_folder = plots_folder
-  output_object.file = file 
-  output_object.title = title
-endfunction
