679f7d1edfbe0ec26d111282838e2d90d6dfd8cb
Mohammed Ali Eslami <meslami@netrias.com>
2017-12-22 07:40:05 -0500

    Job can now be submitted to TACC and it seems to run effectively. There seems to be an internal pyquant error that seems to not like the hackathon data. I am uploading the data I used to test it on to Agave and if successful run is shown, then will submit PR.

--- reactors/lcms-pyquant/lcms-pyquant-0.1.0/runner-template.sh
+++ reactors/lcms-pyquant/lcms-pyquant-0.1.0/runner-template.sh
@@ -1,8 +1,8 @@
-CONTAINER_IMAGE="index.docker.io/chrismit7/pyquant"
+CONTAINER_IMAGE="sd2e/lcms-pyquant-0.1.0:latest"
 
 . _util/container_exec.sh
 
 COMMAND='pyQuant'
-PARAMS='--search-file ${search_file}'
+PARAMS='--search-file ${search_file} --isobaric-tags --label-method TMT10 --no-ratios -o ${output}'
 
 container_exec ${CONTAINER_IMAGE} ${COMMAND} ${PARAMS}
