eb9e24725fd2cc676acee3f178e2fcf6b3360ace
JoshuaUrrutia <joshuau490@gmail.com>
2018-02-07 16:18:43 -0600

    Update runner-template.sh
    fixed parentheses

--- reactors/lcms-pyquant/lcms-pyquant-0.1.1/runner-template.sh
+++ reactors/lcms-pyquant/lcms-pyquant-0.1.1/runner-template.sh
@@ -1,3 +1,5 @@
+#/bin/bash
+
 CONTAINER_IMAGE="sd2e/lcms-pyquant-0.1.1:latest"
 
 . _util/container_exec.sh
@@ -7,32 +9,32 @@ COMMAND='pyQuant'
 PARAMS=''
 if [ -n "${search_file}" ]
 then
-    PARAMS='${PARAMS} --search-file ${search_file}'
+    PARAMS=${PARAMS}' --search-file '${search_file}
 fi
 
 if [ -n "${scan_file_dir}" ]
 then
-    PARAMS='${PARAMS} --scan-file-dir ${scan_file_dir}'
+    PARAMS=${PARAMS}' --scan-file-dir '${scan_file_dir}
 fi
 
 if [ -n "${scan_file}" ]
 then
-    PARAMS='${PARAMS} --scan-file ${scan_file}'
+    PARAMS=${PARAMS}' --scan-file '${scan_file}
 fi
 
 if [ -n "${label_method}" ]
 then
-    PARAMS='${PARAMS} --label-method ${label_method}'
+    PARAMS=${PARAMS}' --label-method '${label_method}
 fi
 
 if [ -n "${isobaric_tags}" ]
 then
-    PARAMS='${PARAMS} --isobaric-tags'
+    PARAMS=${PARAMS}' --isobaric-tags'
 fi
 
 if [ -n "${html_output}" ]
 then
-    PARAMS='${PARAMS} --html'
+    PARAMS=${PARAMS}' --html'
 fi
 
 echo ${PARAMS}
